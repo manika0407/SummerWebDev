03a96a294c19ee1eaced58d6ca50038c3691b9d4
Manika Sharma <manikasharma2211@gmail.com>
2017-07-07 20:14:41 -0700

    Assignment 5 checking heroku with mongodb

--- assignment/app.js
+++ assignment/app.js
@@ -10,14 +10,6 @@ mongoose.Promise = require('q').Promise;
 
 
 var connectionString = 'mongodb://localhost/webdev_summer_2017'; // for local
-if(process.env.MLAB_USERNAME && process.env.MLAB_PASSWORD) {
-    connectionString = process.env.MLAB_USERNAME + ":" +
-        process.env.MLAB_PASSWORD + "@" +
-        process.env.MLAB_HOST + ':' +
-        process.env.MLAB_PORT + '/' +
-        process.env.MLAB_APP_NAME;
-}
-
 
 mongoose.connect(connectionString);
 
